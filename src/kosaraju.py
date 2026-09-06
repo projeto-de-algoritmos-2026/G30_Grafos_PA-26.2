4e28d3cf8e438f0c7bc3d97920dee3180e7505e2
Potatoyz908 <euller2005@gmail.com>
2026-09-06 17:30:48 -0300

    feat: implementa algoritmo de kosaraju

--- src/kosaraju.py
+++ src/kosaraju.py
@@ -33,3 +33,34 @@ class KosarajuGraph:
             for v in self.graph.get(u, []):
                 g_transposed.add_edge(v, u)
         return g_transposed
+
+    def get_sccs(self):
+        """
+        Executa o algoritmo de Kosaraju para encontrar
+        os Componentes Fortemente Conectados (CFCs).
+        """
+        stack = []
+        visited = set()
+        
+        # 1. Primeira DFS para preencher a pilha com a ordem de término
+        for i in self.vertices:
+            if i not in visited:
+                self.dfs(i, visited, stack=stack)
+                
+        # 2 e 3. Obter o grafo transposto (reverso)
+        g_transposed = self.get_transpose()
+        
+        # Preparação para a segunda DFS
+        visited.clear()
+        sccs = []
+        
+        # 4. Segunda DFS no grafo transposto processando na ordem de término (desempilhando)
+        while stack:
+            v = stack.pop()
+            if v not in visited:
+                component = []
+                g_transposed.dfs(v, visited, component=component)
+                # 5. Identificação e armazenamento do CFC
+                sccs.append(component)
+                
+        return sccs
